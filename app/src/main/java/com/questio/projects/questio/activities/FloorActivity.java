d9bab9d2ec298a3af271698608955be31b13a1a8
coad4u4ever <mynamecoad@gmail.com>
2015-04-22 19:49:30 +0700

    flooract

---
+++ app/src/main/java/com/questio/projects/questio/activities/FloorActivity.java
@@ -0,0 +1,15 @@
+package com.questio.projects.questio.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by coad4u4ever on 22-Apr-15.
+ */
+public class FloorActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
