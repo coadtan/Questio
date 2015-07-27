cda8dfad306ce20e4c876b46250a0fbb9a89ce70
coad4u4ever <mynamecoad@gmail.com>
2015-07-27 23:13:13 +0700

    [SP5][DF] Quest status show incorrectly #1

--- app/src/main/java/com/questio/projects/questio/adepters/QuestInActionAdapter.java
+++ app/src/main/java/com/questio/projects/questio/adepters/QuestInActionAdapter.java
@@ -1,12 +1,13 @@
 package com.questio.projects.questio.adepters;
 
+import java.util.ArrayList;
+
 import android.content.Context;
 import android.graphics.Typeface;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -16,17 +17,13 @@ import com.questio.projects.questio.models.Quest;
 import com.questio.projects.questio.models.QuestStatusAndScore;
 import com.questio.projects.questio.utilities.QuestioConstants;
 
-import java.util.ArrayList;
 
-/**
- * Created by coad4u4ever on 05-Apr-15.
- */
-public class QuestInActionAdapter extends ArrayAdapter<Quest> {
-
+public class QuestInActionAdapter extends BaseAdapter {
     public static final String LOG_TAG = QuestInActionAdapter.class.getSimpleName();
     private Context mContext;
     private Typeface tf;
     ArrayList<QuestStatusAndScore> statusList;
+    ArrayList<Quest> quests;
 
     private static class ViewHolder {
         private ImageView questtype;
@@ -52,85 +49,100 @@ public class QuestInActionAdapter extends ArrayAdapter<Quest> {
 
         }
 
+        @Override
+        public String toString() {
+            return questid.getText().toString();
+        }
     }
 
     public QuestInActionAdapter(Context context, ArrayList<Quest> quests, ArrayList<QuestStatusAndScore> statusList) {
-        super(context, 0, quests);
+        this.quests = quests;
         this.statusList = statusList;
         mContext = context;
         tf = Typeface.createFromAsset(context.getAssets(), "fonts/CSPraJad.otf");
     }
 
     @Override
+    public int getCount() {
+        return this.quests.size();
+    }
+
+    @Override
+    public Quest getItem(int position) {
+        return this.quests.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return this.quests.get(position).getQuestId();
+    }
+
+    @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-
-        Quest items = getItem(position);
-        if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item_quest, parent, false);
+        View view = convertView;
+        if (view == null) {
+            view = LayoutInflater.from(mContext).inflate(R.layout.list_item_quest, parent, false);
         }
+        Quest items = getItem(position);
+        if (items != null) {
+            ViewHolder viewHolder = new ViewHolder(view);
+            viewHolder.questid.setText(Integer.toString(items.getQuestId()));
+            viewHolder.questid.setTypeface(tf);
+            viewHolder.questname.setText(items.getQuestName());
+            viewHolder.questname.setTypeface(tf);
+            viewHolder.questdetails.setText(items.getQuestDetails());
+            viewHolder.questTypeInvisible.setText(Integer.toString(items.getQuestTypeId()));
+            viewHolder.zoneid.setText(Integer.toString(items.getZoneId()));
+            viewHolder.questListItem.setBackgroundColor(mContext.getResources().getColor(R.color.grey_500));
+            viewHolder.questListItem.setBackgroundResource(R.drawable.touch_selector);
+            viewHolder.status.setImageResource(R.drawable.ic_quest_unfinish);
 
-
-        ViewHolder viewHolder = new ViewHolder(convertView);
-        viewHolder.questid.setText(Integer.toString(items.getQuestId()));
-        viewHolder.questid.setTypeface(tf);
-        viewHolder.questname.setText(items.getQuestName());
-        viewHolder.questname.setTypeface(tf);
-        viewHolder.questdetails.setText(items.getQuestDetails());
-        viewHolder.questTypeInvisible.setText(Integer.toString(items.getQuestTypeId()));
-        viewHolder.zoneid.setText(Integer.toString(items.getZoneId()));
-
-        if (statusList != null) {
-            for (QuestStatusAndScore q : statusList) {
-                if (items.getQuestId() == q.getQuestId()) {
-                    if (q.getStatus() == QuestioConstants.QUEST_FINISHED || q.getStatus() == QuestioConstants.QUEST_FAILED) {
-                        viewHolder.questListItem.setBackgroundColor(mContext.getResources().getColor(R.color.grey_500));
-                        viewHolder.status.setImageResource(R.drawable.ic_quest_finish);
+            if (this.statusList != null) {
+                for (QuestStatusAndScore q : this.statusList) {
+                    if (items.getQuestId() == q.getQuestId()) {
+                        if (q.getStatus() == QuestioConstants.QUEST_FINISHED || q.getStatus() == QuestioConstants.QUEST_FAILED) {
+                            viewHolder.questListItem.setBackgroundColor(mContext.getResources().getColor(R.color.grey_500));
+                            viewHolder.status.setImageResource(R.drawable.ic_quest_finish);
+                        }
                     }
                 }
             }
+            switch (items.getQuestTypeId()) {
+                case 1:
+                    viewHolder.questtype.setImageResource(R.drawable.ic_icon_quiz);
+                    break;
+                case 2:
+                    viewHolder.questtype.setImageResource(R.drawable.ic_icon_riddle);
+                    break;
+                case 3:
+                    viewHolder.questtype.setImageResource(R.drawable.ic_icon_puzzle);
+                    break;
+            }
+
+            switch (items.getDiffId()) {
+                case 1:
+                    viewHolder.difficulty.setBackgroundColor(mContext.getResources().getColor(R.color.quest_veryeasy));
+                    break;
+                case 2:
+                    viewHolder.difficulty.setBackgroundColor(mContext.getResources().getColor(R.color.quest_easy));
+                    break;
+                case 3:
+                    viewHolder.difficulty.setBackgroundColor(mContext.getResources().getColor(R.color.quest_normal));
+                    break;
+                case 4:
+                    viewHolder.difficulty.setBackgroundColor(mContext.getResources().getColor(R.color.quest_hard));
+                    break;
+                case 5:
+                    viewHolder.difficulty.setBackgroundColor(mContext.getResources().getColor(R.color.quest_veryhard));
+                    break;
+            }
         }
 
-        Log.d(LOG_TAG, "getView: questname = " + items.getQuestName());
-        switch (items.getQuestTypeId()) {
-            case 1:
-                viewHolder.questtype.setImageResource(R.drawable.ic_icon_quiz);
-                break;
-            case 2:
-                viewHolder.questtype.setImageResource(R.drawable.ic_icon_riddle);
-                break;
-            case 3:
-                viewHolder.questtype.setImageResource(R.drawable.ic_icon_puzzle);
-                break;
-        }
-
-        switch (items.getDiffId()) {
-            case 1:
-                viewHolder.difficulty.setBackgroundColor(mContext.getResources().getColor(R.color.quest_veryeasy));
-                break;
-            case 2:
-                viewHolder.difficulty.setBackgroundColor(mContext.getResources().getColor(R.color.quest_easy));
-                break;
-            case 3:
-                viewHolder.difficulty.setBackgroundColor(mContext.getResources().getColor(R.color.quest_normal));
-                break;
-            case 4:
-                viewHolder.difficulty.setBackgroundColor(mContext.getResources().getColor(R.color.quest_hard));
-                break;
-            case 5:
-                viewHolder.difficulty.setBackgroundColor(mContext.getResources().getColor(R.color.quest_veryhard));
-                break;
-        }
-
-//        viewHolder.questtype.setImageDrawable();
-//        viewHolder.difficulty.setImageDrawable();
-//        viewHolder.status.setImageDrawable();
-        return convertView;
+        return view;
     }
 
     @Override
     public void notifyDataSetChanged() {
-        Log.d(LOG_TAG, "notifyDataSetChanged: called");
-
         super.notifyDataSetChanged();
     }
 }
